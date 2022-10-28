f6bf7f5d551c740aa7ec04da27b08efd25f2e259
btxone <btxpablo@gmail.com>
2022-10-28 06:33:43 -0300

    first commit

---
+++ api/src/routes/index.js
@@ -0,0 +1,21 @@
+const { Router } = require('express');
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+const getCountries = require('./getCountries.js')
+const getCountryId = require('./getCountryId.js')
+const postActivity = require('./postActivity.js')
+const getActivities = require('./getActivities.js')
+const delActivity = require('./delActivity.js')
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+router.use('/countries', getCountryId)
+router.use('/countries', getCountries)
+router.use('/activity', postActivity)
+router.use('/activity', getActivities)
+router.use('/delactivity', delActivity)
+
+module.exports = router;
